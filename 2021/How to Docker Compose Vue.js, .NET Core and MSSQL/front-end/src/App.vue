d1e620a040e685daacdc5a18923eb0854bb11dc7
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-03-11 00:40:03 +0200

    Add front-end styling and data fetching

--- 2021/How to Docker Compose Vue.js, .NET Core and MSSQL/front-end/src/App.vue
+++ 2021/How to Docker Compose Vue.js, .NET Core and MSSQL/front-end/src/App.vue
@@ -1,27 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container mt-5">
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Phone Number</th>
+          <th scope="col">Address</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="phoneBookEntry in phoneBookEntries" :key="phoneBookEntry.Id">
+          <td>{{ phoneBookEntry.Id }}</td>
+          <td>{{ phoneBookEntry.firstName }}</td>
+          <td>{{ phoneBookEntry.lastName }}</td>
+          <td>{{ phoneBookEntry.phoneNumber }}</td>
+          <td>{{ phoneBookEntry.address }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, onMounted, ref } from "vue";
+export default defineComponent({
+  name: "Home",
+  setup() {
+    const phoneBookEntries = ref();
 
-@Options({
-  components: {
-    HelloWorld,
+    onMounted(async () => {
+      const url = `${process.env.VUE_APP_API_URL}/api/phone-book`;
+      phoneBookEntries.value = await fetch(url);
+
+      console.log(url);
+      console.log(phoneBookEntries.value);
+    });
+
+    return { phoneBookEntries };
   },
-})
-export default class App extends Vue {}
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
