296aaf9f7b1d264df700aa69336b8733c72ff18b
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-03-10 10:53:26 +0200

    Add barebones backend project

---
+++ 2021/How to Docker Compose Vue.js, .NET Core and MSSQL/Backend/Backend/Startup.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Backend
+{
+    public class Startup
+    {
+        private readonly IConfigurationRoot _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.Development.json", optional: true)
+              .AddJsonFile("appSettings.json", optional: true)
+              .AddEnvironmentVariables()
+              .Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Backend", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
