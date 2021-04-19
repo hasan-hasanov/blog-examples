6f80ccf37d7536981b77d601832719076dbb6ff6
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-04-19 14:48:12 +0300

    Add ble blog post example

---
+++ 2021/How to Use BLE With WPF or Forms/BleConsoleDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+
+namespace BleConsoleDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var watcher = new BluetoothLEAdvertisementWatcher();
+            watcher = new BluetoothLEAdvertisementWatcher()
+            {
+                ScanningMode = BluetoothLEScanningMode.Passive
+            };
+
+            watcher.Received += Watcher_Received;
+            watcher.Start();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private static async void Watcher_Received(
+            BluetoothLEAdvertisementWatcher sender,
+            BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+            var device = await BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);
+            if (device != null)
+            {
+                Console.WriteLine(device.BluetoothAddress);
+            }
+        }
+    }
+}
